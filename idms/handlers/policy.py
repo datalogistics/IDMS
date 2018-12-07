b9cfcdfd3e648dc9d74e94234c4d261243539e3d
Jeremy Musser <jemusser@indiana.edu>
2018-12-07 13:46:12 -0500

    Resolved issue wherein errors in raw policy posts were incorrectly returned

--- idms/handlers/policy.py
+++ idms/handlers/policy.py
@@ -17,11 +17,13 @@ class PolicyHandler(_BaseHandler):
         except AssertionError as exp:
             resp.body = json.dumps({"errorcode": 1, "msg": str(exp)})
             resp.status = falcon.HTTP_401
+            return
         try:
             policy_id = Policy(body["$match"], body["$action"])
         except Exception as exp:
             resp.body = json.dumps({"errorcode": 2, "msg": "Malformed policy - {}".format(exp)})
             resp.status = falcon.HTTP_401
+            return
          
         resp.body = json.dumps({"errorcode": None, "msg": "", "policyid": policy_id})
         resp.status = falcon.HTTP_200
