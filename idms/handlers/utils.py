c87758b4f37efa2917a72553a5dc74246a1c4d18
Jeremy Musser <jemusser@iu.edu>
2019-06-15 20:04:36 -0400

    Added bounded stream to prevent out-of-bounds errors on some clients

--- idms/handlers/utils.py
+++ idms/handlers/utils.py
@@ -4,7 +4,7 @@ import json
 def get_body(fn):
     def _f(self, req, *args, **kwargs):
         if req.content_length:
-            kwargs['body'] = json.loads(req.stream.read().decode('utf-8'))
+            kwargs['body'] = json.loads(req.bounded_stream.read().decode('utf-8'))
         fn(self, req, *args, **kwargs)
     
     return _f
