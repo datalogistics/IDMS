5b09db4510420f903c60d68b8b7c471524f6311f
Jeremy Musser <jemusser@iu.edu>
2019-06-20 14:49:16 -0400

    Prevented failure during unregistered runtime insertion

--- idms/lib/service.py
+++ idms/lib/service.py
@@ -1,3 +1,4 @@
+from unis.exceptions import UnisReferenceError
 from unis.services import RuntimeService
 from unis.services.event import new_update_event
 from unis.models import Service
@@ -10,7 +11,9 @@ class IDMSService(RuntimeService):
 
     @new_update_event(["services", "exnodes"])
     def new(self, resource):
-        if resource.getSource() != self._master: return
+        try:
+            if resource.getSource() != self._master: return
+        except UnisReferenceError: return
         
         if isinstance(resource, Service):
             if resource.id not in self._known or resource.ts - self._known[resource.id] > getattr(resource, 'ttl', 0):
